c559ffc3521146e7797215933b3bf08ab97e6947
raouftams <raouftams@gmail.com>
2021-03-25 22:37:42 +0100

    Adding main class

---
+++ src/MainProg.java
@@ -0,0 +1,42 @@
+import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class MainProg {
+    public static void main(String[] args) throws Exception {
+        String fileName = null;
+        if(args.length>0){
+            fileName = args[0];
+        }
+        InputStream is = System.in;
+        if(fileName!=null)
+        {
+            is = new FileInputStream(fileName);
+        }
+        CharStream input = new UnbufferedCharStream(is);
+        TinyLangLexer lex = new TinyLangLexer(input);
+        lex.setTokenFactory(new CommonTokenFactory(true));
+
+        //POUR RECUPERER les entitées lexicales
+        TokenStream tokens = new UnbufferedTokenStream<CommonToken>(lex);
+
+        //instanciation de la classe parser avec les tokens reconnu
+        TinyLangParser parser = new TinyLangParser(tokens);
+
+        //realisation d'un arbre syntaxique avec les tokens
+        //program represente la premiere regle de la grammaire
+        ParseTree tree = parser.programme();
+
+        //affichage de l'arbre a l'execution
+        Trees.inspect(tree,parser);
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        //instanciation de la classe listener la classe des routines semantiques
+        Listener list = new Listener();
+        walker.walk(list,tree);
+    }
+}

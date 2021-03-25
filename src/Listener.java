5f7627731a39a631f4aa26c83abfb3cfcea91f1d
raouftams <raouftams@gmail.com>
2021-03-25 22:39:03 +0100

    ExtDec, ExtProg and extAff methods

--- src/Listener.java
+++ src/Listener.java
@@ -11,25 +11,58 @@ import java.util.*;
 public class Listener extends TinyLangBaseListener{
     //l objet table pour pouvoir ajouter dans la ts
     private TS table = new TS();
-    public int nberreur;
+    public int nbErreurs;
+
     //table de hashage pour pouvoir range le type et la regle
     private HashMap<ParserRuleContext,Integer> Ruletype = new HashMap<>();
-    LinkedList<String> erreur = new LinkedList<>();
+
+    LinkedList<String> erreurs = new LinkedList<>();
     //elle nous permet de parcourir l'arbre et a chaque regle on fait un traitement
     //ajout dans la ts par exemple
     //exitdv parceque je doit recuperer le type 0 si float et 1  sinon
 
 
     @Override
-    public void enterAffectation(TinyLangParser.AffectationContext ctx) {
-        super.enterAffectation(ctx);
+    public void exitProgramme(TinyLangParser.ProgrammeContext ctx) {
+        if (this.erreurs.size() == 0){
+            //Aucune erreur dans le programme
+            this.nbErreurs = 0;
+            System.out.println("\nLa table des symboles");
+            this.table.afficheTS();
 
+        } else {
+            // Dans le cas où il y a des erreurs
+            for ( String error: this.erreurs ) {
+                System.out.println(error);
+            }
+        }
+    }
+
+    @Override
+    public void exitDeclaration(TinyLangParser.DeclarationContext ctx) {
+        TinyLangParser.TypeContext typeVar = ctx.type();
+        TinyLangParser.IdentifiantsContext varsCtx = ctx.identifiants();
+        String vars[] = varsCtx.getText().split(",");
+        for ( String v : vars) {
+            //instanciation d'un element
+            TS.Element element = new TS.Element(v, typeVar.getText(), null, 1);
+
+            //Ajout à la table des symboles
+            if (table.containsElement(v) == false){
+                table.addElement(element);
+            }else{
+                this.erreurs.add("Erreur: double déclaration de la variable " + v);
+            }
+        }
     }
 
     @Override
     public void exitAffectation(TinyLangParser.AffectationContext ctx) {
-        super.exitAffectation(ctx);
-        ctx.getText();
-
+        if (ctx.IDENTIFIANT() != null){
+            if (!table.containsElement(ctx.IDENTIFIANT().getText())){
+                this.erreurs.add("Erreur: la variable " + ctx.IDENTIFIANT().getText() + " n'est pas declaree");
             }
+        }
+
+    }
 }
